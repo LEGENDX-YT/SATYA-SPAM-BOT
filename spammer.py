d2d40de0c409d3fc61dd775ad4fe0b33c9150037
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-04 07:35:36 -0700

    baap

---
+++ spammer.py
@@ -0,0 +1,14 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://gitHub.com/SATYA-OP/SATYA-SPAM-BOT && cd SATYA-SPAM-BOT && python3 -m SatyaSpammer")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Its_LegendBoy")
+# Script written for stop heroku suspension
+# use with credit else gay
+#By @LegendHacker_IIN (t.me/LegendHacker_IIN) For Fixing Heroku Suspension
